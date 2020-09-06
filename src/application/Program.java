46897ed17d5a56438e5d8919d5a005e656e5e061
Tiago <tiagohs.199@gmail.com>
2020-09-05 22:05:01 -0300

    creating class department

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Deparment;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Deparment obj = new Deparment(1,"books");
+		System.out.println(obj);
+
+	}
+
+}
